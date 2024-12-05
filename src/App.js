6732736cede2501810ffeacb81cc80b2d7af72d7
johmik1997 <yohannesgeta9@gmail.com>
2024-12-04 21:43:17 -0600

    the added of the ui display, close button and check if it is unpacked

--- src/App.js
+++ src/App.js
@@ -5,11 +5,19 @@ const initialItems = [
   { id: 2, description: "Socks", quantity: 12, packed: true },
 ];
 export default function App() {
+  const [items, setItem] = useState([]);
+  function handleAdditem(item){
+    setItem((items)=>[...items,item])
+    
+      }
+      function handleDelete(id){
+        setItem(items=>items.filter(item=>id!==item.id))
+      }
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <ItemList />
+      <Form handleAdditem={handleAdditem}/>
+      <ItemList item={items} handleDelete={handleDelete}/>
       <Stats />
     </div>
   );
@@ -18,57 +26,67 @@ export default function App() {
 function Logo() {
   return <h1>🌴FAR AWAY 💼</h1>;
 }
-function Form() {
+function Form({handleAdditem}) {  
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(0);
+  
+
+
+
   function handleSubmit(e) {
     e.preventDefault();
+    if(!description)
+      return;
     const newarr = { description, quantity, id: Date.now(), packed: false };
-    const newpacked = { ...initialItems, newarr };
-    console.log(newpacked);
+    //const newpacked = { ...initialItems, newarr };
+    handleAdditem(newarr);
+    setDescription('');
+    setQuantity(1);
+
   }
-  return (
-    <form className="add-form" onSubmit={handleSubmit}>
-      <h3>what do you need😍 for your trip?</h3>
-      <select
-        value={quantity}
-        onChange={(val) => setQuantity(Number(val.target.value))}
-      >
-        {Array.from({ length: 20 }, (_, i) => i + 1).map((i) => (
-          <option value={i} key={i}>
-            {i}
-          </option>
-        ))}
-      </select>
-      <input
-        type="text"
-        placeholder="items..."
-        value={description}
-        onChange={(val) => setDescription(val.target.value)}
-      ></input>
-      <button>add</button>
-    </form>
-  );
+  return ( <form className="add-form" onSubmit={handleSubmit}>
+    <h3>what do you need😍 for your trip?</h3>
+    <select
+      value={quantity}
+      onChange={(val) => setQuantity(Number(val.target.value))}
+    >
+      {Array.from({ length: 20 }, (_, i) => i + 1).map((i) => (
+        <option value={i} key={i}>
+          {i}
+        </option>
+      ))}
+    </select>
+    <input
+      type="text"
+      placeholder="items..."
+      value={description}
+      onChange={(val) => setDescription(val.target.value)}
+    ></input>
+    <button>add</button>
+  </form>
+);
 }
 
-function ItemList() {
+function ItemList({item, handleDelete}) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((items) => (
-          <Item items={items} key={items.id} />
+        {item.map((items) => (
+          <Item items={items} handleDelete={handleDelete} key={items.id} />
         ))}
       </ul>
     </div>
   );
 }
-function Item({ items }) {
+function Item({ items,handleDelete }) {
+  const[packed,setPacked]=useState(false);
   return (
     <li>
-      <span style={items.packed ? { textDecoration: "line-through" } : {}}>
+      <input type="checkBox" value={packed} onChange={(val)=>setPacked(val.target.checked)}/>
+      <span style={packed ? { textDecoration: "line-through" } : {}}>
         {items.quantity} {items.description}
       </span>
-      <button>❌</button>
+      <button onClick={()=>handleDelete(items.id)}>❌</button>
     </li>
   );
 }
--- src/flashCard.js
+++ src/flashCard.js
@@ -1,49 +1,49 @@
 
 
-import React, { useState } from 'react';
-import './flashes.css';
-export default function App(){
-    return <Flash />
-}
-const question=[
-    {id:1,
-    question:'what language is react based on?',
-    answer:'javascript'
-    },
-    {
-        id:2,
-        question:"what are the building block of react app?",
-        answer:"component"
-    },{
-        id:3,
-        question:"what is the name of the syntax we use to describe a Ui in react?",
-        answer:'JSX'
-    },{
-        id:4,
-        question:"how to pass data from parents to child components",
-        answer:'props'
-    },{
-        id:5,
-        question:"how to give components memory?",
-        answer:'state'
-    },{
-        id:6,
-        question:"what do we call an input element that is completely synchronized with state?",
-        answer:'control element'
-    }]
-function Flash(){
-    const [selectedId, setSelectedid]=useState(1);
-function handleClick(id){
-  setSelectedid(id!==selectedId?id:null);
-}
-return (<div className='flashs' >
- 
-    {question.map((question)=>(
-      <div className={selectedId==question.id?'answers':'flashes'} 
-      key={question.id}  onClick={()=>handleClick(question.id)}>
-        
-        <p>{selectedId==question.id?question.answer:question.question}</p>
-        </div>))}
+// import { useState,React  } from 'react';
+// import './flashes.css';
 
-</div>)
-}
+
+// function flashCard(){
+//     const question=[
+//         {id:1,
+//         question:'what language is react based on?',
+//         answer:'javascript'
+//         },
+//         {
+//             id:2,
+//             question:"what are the building block of react app?",
+//             answer:"component"
+//         },{
+//             id:3,
+//             question:"what is the name of the syntax we use to describe a Ui in react?",
+//             answer:'JSX'
+//         },{
+//             id:4,
+//             question:"how to pass data from parents to child components",
+//             answer:'props'
+//         },{
+//             id:5,
+//             question:"how to give components memory?",
+//             answer:'state'
+//         },{
+//             id:6,
+//             question:"what do we call an input element that is completely synchronized with state?",
+//             answer:'control element'
+//         }]
+//     const [selectedId, setSelectedid]=useState(1);
+// function handleClick(id){
+//   setSelectedid(id!==selectedId?id:null);
+// }
+// return (<div className='flashs' >
+ 
+//     {question.map((question)=>(
+//       <div className={selectedId==question.id?'answers':'flashes'} 
+//       key={question.id}  onClick={()=>handleClick(question.id)}>
+        
+//         <p>{selectedId==question.id?question.answer:question.question}</p>
+//         </div>))}
+
+// </div>)
+// }
+// export default flashCard;
